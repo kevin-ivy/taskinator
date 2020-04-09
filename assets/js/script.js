638a1ce3fedb70a33d0f0155bab3b2815415c39c
kIvy1189 <kevindivy@gmail.com>
2020-04-09 12:55:00 -0500

    Added highlight for draggable areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -196,6 +196,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest('.task-list');
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -218,7 +219,16 @@ var dropTaskHandler = function(event){
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+};
+
+//Function for drag leave function
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest('.task-list');
+    if (taskListEl){
+        taskListEl.removeAttribute('style');
+    }
 };
 
 
@@ -234,3 +244,5 @@ pageContentEl.addEventListener('dragstart', dragTaskHandler);
 pageContentEl.addEventListener('dragover', dropZoneDragHandler);
 // define drop handling
 pageContentEl.addEventListener('drop', dropTaskHandler);
+// define drop leave handling
+pageContentEl.addEventListener('dragleave', dragLeaveHandler);
